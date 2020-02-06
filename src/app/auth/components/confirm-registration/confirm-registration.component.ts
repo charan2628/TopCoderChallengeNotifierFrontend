1ebb528b7ecf5ca9d5ad50987b47e8d136819c7a
charan <s.charancherry22@gmail.com>
2020-02-06 08:49:32 +0530

    Integrated AuthService to ConfirmRegistration component

--- src/app/auth/components/confirm-registration/confirm-registration.component.ts
+++ src/app/auth/components/confirm-registration/confirm-registration.component.ts
@@ -1,19 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-confirm-registration',
   templateUrl: './confirm-registration.component.html',
   styleUrls: ['./confirm-registration.component.css']
 })
-export class ConfirmRegistrationComponent {
+export class ConfirmRegistrationComponent implements OnInit {
 
   confirmForm: FormGroup;
+  submitting = false;
+  message: String;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router,
+              private auth: AuthService, private route: ActivatedRoute) {
     this.confirmForm = this.fb.group({
+      'email': [null, Validators.required],
       "confirm-code": [null, Validators.required]
     });
    }
+
+   ngOnInit() {
+    this.email.setValue(this.route.snapshot.queryParams['email']);
+   }
+
+   onSubmit() {
+     if(this.confirmForm.valid) {
+       this.submitting = true;
+        this.auth.confirmRegistration(this.email.value, this.confirmCode.value).subscribe(
+          res => {
+            this.router.navigate(['/login'], {queryParams: {email: this.email.value}});
+          },
+          err => {
+            this.message = "Invalid code"
+            this.submitting = false;
+          }
+        )
+     }
+   }
+
+   get email() { return this.confirmForm.get('email'); }
+
+   get confirmCode() { return this.confirmForm.get('confirm-code'); }
 }
