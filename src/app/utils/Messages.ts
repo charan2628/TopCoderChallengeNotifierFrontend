af07617a37d921ac5e3ff96451e9f94ce6ef848d
charan <s.charancherry22@gmail.com>
2020-02-06 23:50:16 +0530

    Added constant server messages

---
+++ src/app/utils/Messages.ts
@@ -0,0 +1,9 @@
+export enum Message {
+    INVALID_CONFIRMATION_CODE = "Invalid confirmation code.",
+    USER_ALREADY_PRESENT = "User already present.",
+    CHECK_YOUR_CONNECTION = "Please check your network connnection.",
+    ERROR_OCCURED = "An Error occured, please try again.",
+    IVALID_EMAIL_OR_PASSWORD = "Invalid userame or password.",
+    USER_NOT_CONFIRMED = "User is not confirmed.",
+    UNCOFIRMED_REGISTRATION = "Unconfirmed registration."
+}
