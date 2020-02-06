ccb6da9c4b818875b2906c0204a33b3b416b2f69
charan <s.charancherry22@gmail.com>
2020-02-06 22:43:49 +0530

    Designed and Integrated Logout Component

---
+++ src/app/auth/components/logout/logout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-logout',
+  template: ''
+})
+export class LogoutComponent implements OnInit {
+
+  constructor(private router: Router, private auth: AuthService) { }
+
+  ngOnInit() {
+    this.auth.logOut();
+    this.router.navigate(['login']);
+  }
+
+}
