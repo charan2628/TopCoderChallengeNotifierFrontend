9e96b6707f9117df3c94eb40b8ce8eb5c62c7413
charan <s.charancherry22@gmail.com>
2020-02-06 09:05:53 +0530

    Integrated AuthService to Login Component

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,21 +1,46 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  submitting = false;
+  message: String;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router,
+              private auth: AuthService, private route: ActivatedRoute) {
     this.loginForm = this.fb.group({
       email: [null, Validators.required],
       password: [null, Validators.required]
     });
    }
 
+   ngOnInit() {
+    this.email.setValue(this.route.snapshot.queryParams['email']);
+   }
+
+   onSubmit() {
+     if(this.loginForm.valid) {
+       this.submitting = true;
+       this.auth.login(this.loginForm.getRawValue()).subscribe(
+         res => this.router.navigate(['user-dashboard']),
+         err => {
+          this.message = "Invalid username or password";
+          this.submitting = false;
+         }
+       )
+     }
+   }
+
+   get email() { return this.loginForm.get('email'); }
+
+   get password() { return this.loginForm.get('password'); }
 }
