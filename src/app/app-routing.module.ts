b3398a01575473261c365d887c31bfa7b14023d5
charan <s.charancherry22@gmail.com>
2020-02-06 22:44:12 +0530

    Updated routes for new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from './guards/auth.guard';
-import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { ConfirmRegistrationComponent } from './auth/components/confirm-registration/confirm-registration.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminLoginComponent } from './auth/components/admin-login/admin-login.component';
+import { LogoutComponent } from './auth/components/logout/logout.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: '', pathMatch: 'full', redirectTo: 'dashboard'
+    path: '', pathMatch: 'full', redirectTo: 'register'
   },
-  {
-    path: 'login', component: LoginComponent
-  },
-  {
-    path: 'dashboard',
-    canActivate: [AuthGuard],
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-  }
+  { path: 'register', component: RegisterComponent },
+  { path: 'confirm-registration', component: ConfirmRegistrationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin-login', component: AdminLoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'user-dashboard', canActivate: [AuthGuard], component: UserDashboardComponent },
+  { path: 'admin-dashboard', canActivate: [AuthGuard], component: AdminDashboardComponent }
 ];
 
 @NgModule({
